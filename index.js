b096e4dace640ea5c4b465b88519cddd03ece800
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-04-09 17:46:13 -0400

    added other s tuff

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.get("/", (req, res) => {
     <p>Welcome to the Lambda Hubs API</p>
   `);
 });
-
-server.listen(5000, () => {
-  console.log("\n*** Server Running on http://localhost:5000 ***\n");
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
